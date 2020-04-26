da142319fda4bccdf3dc9032fbc2388de0287eca
Morning~StAr <63952441+MoRniNg-stAr-Eloy@users.noreply.github.com>
2020-04-26 13:34:37 +0530

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,6 +1,6 @@
-read -p "What Bash Welcome Screen You Want ? (1/2..)" PROCS
+read -p "What Bash Welcome Screen You Want ? (1/2..)" PROC
 tput sgr 0
-if ["$PROCS" = "1"]; then
+if ["$PROC" = "1"]; then
 rm -rf bash.bashrc /data/data/com.termux/files/home/bash.bashrc
 sleep 2
 else 
