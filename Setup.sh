8bebe53322a828101b2a30d5c725b27c83e6015c
Morning~StAr <63952441+MoRniNg-stAr-Eloy@users.noreply.github.com>
2020-04-26 13:23:31 +0530

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,38 +1,13 @@
 read -p "What Bash Welcome Screen You Want ? (1/2..)" PROCS
 tput sgr 0
 if ["$PROCS" = "1"]; then
-if [ -e $HOME/bash.bashrc]; then
-rm $HOME/bash.bashrc;
+rm -rf bash.bashrc /data/data/com.termux/files/home/bash.bashrc
 sleep 2
-
-
-rm -rf $PREFIX/etc/bash.bashrc;
-fi
-sleep 1
-
-if [ -e $HOME/bash.bashrc]; then
-echo '\033[1;91m'bash not removed from home'\033[1;97m';
-else
-echo '\033[1;91m'Bash Successfully Removed'\033[1;97m';
-sleep 1
-wget https://raw.githubusercontent.com/MoRniNg-stAr-Eloy/Custom-bash/master/bash.bashrc &> /dev/null & spin; 
-fi
-sleep 2
-if [ -e $PREFIX/etc/bash.bashrc]; then
-echo '\033[1;91m'bash not removed fom Main'\033[1;97m';
-rm -rf $PREFIX/etc/bash.bashrc;
-else
-echo '\033[1;91m'bash Successfully removed from home'\033[1;97m';
-fi
-sleep 2
-
-if [ -e $PREFIX/etc/bash.bashrc]; then
-echo "";
-rm -rf $PREFIX/etc/bash.bashrc
-else
-cd $HOME
-cat bash.bashrc >> $PREFIX/etc/bash.bashrc
-fi
+else 
+rm -rf bash.bashrc /data/data/com.termux/files/usr/etc/bash.bashrc
+done
 exit 0
+
+
 
 
